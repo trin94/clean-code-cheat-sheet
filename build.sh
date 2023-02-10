275b92ac23aae4a6892a04335aee55f5acca6080
Elias Müller <elias.mr1@gmail.com>
2023-02-10 20:52:22 +0100

    🏗️ Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd src || exit
 pdflatex main.tex
@@ -7,4 +7,3 @@ find . -name "*.log" -type f -delete
 find . -name "*.out" -type f -delete
 mv main.pdf ../main.pdf
 cd .. || exit
-
